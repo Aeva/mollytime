051a4c57d3a62e11a220d8be9268f59bf05d61ab
Aeva Palecek <aeva.ntsc@gmail.com>
2023-04-21 02:45:08 -0500

    MIDI Device Selection, Debug, and Other Things

--- src/mollytime.cpp
+++ src/mollytime.cpp
@@ -13,7 +13,15 @@
 #include <backends/imgui_impl_sdl2.h>
 #include <backends/imgui_impl_opengl2.h>
 
+#include <fmt/format.h>
+
+#include <chrono>
+#include <string>
+#include <vector>
 #include <iostream>
+
+
+using Clock = std::chrono::high_resolution_clock;
 
 
 enum class StatusCode
@@ -29,6 +37,85 @@ enum class StatusCode
 SDL_Window* Window = nullptr;
 SDL_GLContext Context = nullptr;
 
+snd_seq_t *SeqHandle = nullptr;
+
+
+struct ExtPort
+{
+	int Client;
+	int Port;
+	unsigned int Cap;
+	unsigned int Type;
+	std::string Name;
+	bool Available;
+};
+
+
+std::vector<ExtPort> AllDevices;
+std::vector<ExtPort> ExtInputs;
+std::vector<ExtPort> ExtOutputs;
+
+
+void RefreshDeviceList()
+{
+	AllDevices.clear();
+	ExtInputs.clear();
+	ExtOutputs.clear();
+
+	snd_seq_client_info_t* ClientInfo = nullptr;
+	snd_seq_client_info_alloca(&ClientInfo);
+
+	snd_seq_port_info_t* PortInfo = nullptr;
+	snd_seq_port_info_alloca(&PortInfo);
+
+	snd_seq_client_info_set_client(ClientInfo, -1);
+	while (snd_seq_query_next_client(SeqHandle, ClientInfo) >= 0)
+	{
+		const char* ClientName = snd_seq_client_info_get_name(ClientInfo);
+		int Client = snd_seq_client_info_get_client(ClientInfo);
+		snd_seq_port_info_set_client(PortInfo, Client);
+		snd_seq_port_info_set_port(PortInfo, -1);
+		while (snd_seq_query_next_port(SeqHandle, PortInfo) >= 0)
+		{
+			unsigned int PortCap = snd_seq_port_info_get_capability(PortInfo);
+			const bool Read = (PortCap & SND_SEQ_PORT_CAP_READ) != 0;
+			const bool Write = (PortCap & SND_SEQ_PORT_CAP_WRITE) != 0;
+
+			unsigned int PortType = snd_seq_port_info_get_type(PortInfo);
+			const bool Generic = (PortType & SND_SEQ_PORT_TYPE_MIDI_GENERIC) != 0;
+
+			const char* PortName = snd_seq_port_info_get_name(PortInfo);
+			std::string DeviceName = fmt::format("{} : {}", ClientName, PortName);
+			int Port = snd_seq_port_info_get_port(PortInfo);
+
+			ExtPort Device = { Client, Port, PortCap, PortType, DeviceName, false };
+
+			if (Generic && (Read || Write))
+			{
+				Device.Available = true;
+				if (Read)
+				{
+					ExtInputs.push_back(Device);
+				}
+				if (Write)
+				{
+					ExtOutputs.push_back(Device);
+				}
+			}
+
+			AllDevices.push_back(Device);
+		}
+	}
+}
+
+
+bool ShowDemoWindow = false;
+bool ShowConnections = false;
+bool ShowDeviceDebug = false;
+bool ShowFrameRate = false;
+
+float PresentFrequency = 0.0;
+float PresentDeltaMs = 0.0;
 
 void MainLoop()
 {
@@ -53,13 +140,204 @@ void MainLoop()
 			}
 		}
 
+		Clock::time_point FrameStartTimePoint = Clock::now();
+		double CurrentTime;
+		{
+			static Clock::time_point StartTimePoint = FrameStartTimePoint;
+			static Clock::time_point LastTimePoint = StartTimePoint;
+			{
+				std::chrono::duration<double, std::milli> FrameDelta = FrameStartTimePoint - LastTimePoint;
+				PresentDeltaMs = float(FrameDelta.count());
+			}
+			{
+				std::chrono::duration<double, std::milli> EpochDelta = FrameStartTimePoint - StartTimePoint;
+				CurrentTime = float(EpochDelta.count());
+			}
+			LastTimePoint = FrameStartTimePoint;
+			PresentFrequency = float(1000.0 / PresentDeltaMs);
+		}
+
 		ImGui_ImplOpenGL2_NewFrame();
 		ImGui_ImplSDL2_NewFrame();
 		ImGui::NewFrame();
 
+		if (ImGui::BeginMainMenuBar())
 		{
-			bool ShowDemoWindow = true;
+			if (ImGui::BeginMenu("File"))
+			{
+				if (ImGui::MenuItem("Exit"))
+				{
+					Live = false;
+				}
+				ImGui::EndMenu();
+			}
+			if (ImGui::BeginMenu("Settings"))
+			{
+				if (ImGui::MenuItem("MIDI Ports", nullptr, &ShowConnections))
+				{
+				}
+				ImGui::EndMenu();
+			}
+			if (ImGui::BeginMenu("Debug"))
+			{
+				if (ImGui::MenuItem("Show FPS", nullptr, &ShowFrameRate))
+				{
+				}
+				if (ImGui::MenuItem("ALSA Devices", nullptr, &ShowDeviceDebug))
+				{
+				}
+				if (ImGui::MenuItem("Dear ImGui", nullptr, &ShowDemoWindow))
+				{
+				}
+				ImGui::EndMenu();
+			}
+
+			if (ShowFrameRate)
+			{
+				ImGui::Text(" %.0f hz\n", round(PresentFrequency));
+			}
+
+			ImGui::EndMainMenuBar();
+		}
+
+		if (ShowDemoWindow)
+		{
 			ImGui::ShowDemoWindow(&ShowDemoWindow);
+		}
+
+		if (ShowDeviceDebug || ShowConnections)
+		{
+			RefreshDeviceList();
+		}
+
+		if (ShowDeviceDebug)
+		{
+			int Margin = 0;
+			const ImGuiViewport* MainViewport = ImGui::GetMainViewport();
+			ImGui::SetNextWindowPos(ImVec2(MainViewport->WorkPos.x + Margin, MainViewport->WorkPos.y + Margin), ImGuiCond_Always);
+			ImGui::SetNextWindowSize(ImVec2(MainViewport->WorkSize.x - Margin * 2, MainViewport->WorkSize.y - Margin * 2), ImGuiCond_Always);
+
+			ImGuiWindowFlags WindowFlags = \
+				ImGuiWindowFlags_AlwaysVerticalScrollbar |
+				ImGuiWindowFlags_NoSavedSettings |
+				ImGuiWindowFlags_NoResize |
+				ImGuiWindowFlags_NoMove;
+
+			if (ImGui::Begin("ALSA Device Information", &ShowDeviceDebug, WindowFlags))
+			{
+				ImGui::SeparatorText("MIDI Devices");
+				int i = 0;
+				for (const ExtPort& Device : AllDevices)
+				{
+					ImGui::PushID(i++);
+					if (Device.Available)
+					{
+						ImGui::TextColored(ImVec4(0.0, 1.0, 0.0, 1.0), Device.Name.c_str());
+					}
+					else
+					{
+						ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.5, 0.5, 0.5, 1.0));
+						ImGui::Text(Device.Name.c_str());
+					}
+
+#define PORT_CAP(CAP) \
+					if (Device.Cap & CAP) \
+					{ \
+						ImGui::BulletText(#CAP); \
+					}
+
+					PORT_CAP(SND_SEQ_PORT_CAP_READ);
+					PORT_CAP(SND_SEQ_PORT_CAP_WRITE);
+					PORT_CAP(SND_SEQ_PORT_CAP_SYNC_READ);
+					PORT_CAP(SND_SEQ_PORT_CAP_SYNC_WRITE);
+					PORT_CAP(SND_SEQ_PORT_CAP_DUPLEX);
+					PORT_CAP(SND_SEQ_PORT_CAP_SUBS_READ);
+					PORT_CAP(SND_SEQ_PORT_CAP_SUBS_WRITE);
+					PORT_CAP(SND_SEQ_PORT_CAP_NO_EXPORT);
+#undef PORT_CAP
+
+#define PORT_TYPE(TYPE) \
+					if (Device.Type & TYPE) \
+					{ \
+						ImGui::BulletText(#TYPE); \
+					}
+
+					PORT_TYPE(SND_SEQ_PORT_TYPE_SPECIFIC);
+					PORT_TYPE(SND_SEQ_PORT_TYPE_MIDI_GENERIC);
+					PORT_TYPE(SND_SEQ_PORT_TYPE_MIDI_GM);
+					PORT_TYPE(SND_SEQ_PORT_TYPE_MIDI_GS);
+					PORT_TYPE(SND_SEQ_PORT_TYPE_MIDI_XG);
+					PORT_TYPE(SND_SEQ_PORT_TYPE_MIDI_MT32);
+					PORT_TYPE(SND_SEQ_PORT_TYPE_MIDI_GM2);
+					PORT_TYPE(SND_SEQ_PORT_TYPE_SYNTH);
+					PORT_TYPE(SND_SEQ_PORT_TYPE_DIRECT_SAMPLE);
+					PORT_TYPE(SND_SEQ_PORT_TYPE_SAMPLE);
+					PORT_TYPE(SND_SEQ_PORT_TYPE_HARDWARE);
+					PORT_TYPE(SND_SEQ_PORT_TYPE_SOFTWARE);
+					PORT_TYPE(SND_SEQ_PORT_TYPE_SYNTHESIZER);
+					PORT_TYPE(SND_SEQ_PORT_TYPE_PORT);
+					PORT_TYPE(SND_SEQ_PORT_TYPE_APPLICATION);
+#undef PORT_TYPE
+
+					if (!Device.Available)
+					{
+						ImGui::PopStyleColor(1);
+					}
+					ImGui::PopID();
+				}
+			}
+			ImGui::End();
+		}
+
+		if (ShowConnections)
+		{
+			ImGuiWindowFlags WindowFlags = 0;
+			if (ImGui::Begin("MIDI Device Selection", &ShowConnections, WindowFlags))
+			{
+				static int SelectedInput = 0;
+				static int SelectedOutput = 0;
+
+				if (SelectedInput >= ExtInputs.size())
+				{
+					SelectedInput = 0;
+				}
+
+				if (SelectedOutput >= ExtOutputs.size())
+				{
+					SelectedOutput = 0;
+				}
+
+				ImGui::SeparatorText("Input Device");
+				{
+					ImGui::PushID("MIDI input selection");
+					for (int i = 0; i < ExtInputs.size(); ++i)
+					{
+						ImGui::PushID(i);
+						if (ImGui::RadioButton(ExtInputs[i].Name.c_str(), SelectedInput == i))
+						{
+							SelectedInput = i;
+						}
+						ImGui::PopID();
+					}
+					ImGui::PopID();
+				}
+
+				ImGui::SeparatorText("Output Device");
+				{
+					ImGui::PushID("MIDI output selection");
+					for (int i = 0; i < ExtOutputs.size(); ++i)
+					{
+						ImGui::PushID(i);
+						if (ImGui::RadioButton(ExtOutputs[i].Name.c_str(), SelectedOutput == i))
+						{
+							SelectedOutput = i;
+						}
+						ImGui::PopID();
+					}
+					ImGui::PopID();
+				}
+			}
+			ImGui::End();
 		}
 
 		ImGui::Render();
@@ -145,6 +423,21 @@ StatusCode Boot()
 		ImGui_ImplOpenGL2_Init();
 		std::cout << "Done!\n";
 	}
+	{
+		std::cout << "Creating ALSA client... ";
+		if (snd_seq_open(&SeqHandle, "default", SND_SEQ_OPEN_DUPLEX, SND_SEQ_NONBLOCK) == 0)
+		{
+			snd_seq_set_client_name(SeqHandle, "mollytime");
+			std::cout << "Done!\n";
+		}
+		else
+		{
+			std::cout << "Failed to initialize ALSA.\n";
+			return StatusCode::FAIL;
+		}
+	}
+
+
 	std::cout << "Using device: " << glGetString(GL_RENDERER) << " " << glGetString(GL_VERSION) << "\n";
 	return StatusCode::PASS;
 }
@@ -152,6 +445,10 @@ StatusCode Boot()
 
 void Teardown()
 {
+	if (SeqHandle)
+	{
+		snd_seq_close(SeqHandle);
+	}
 	if (Context)
 	{
 		ImGui_ImplOpenGL2_Shutdown();
