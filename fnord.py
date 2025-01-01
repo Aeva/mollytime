ce7ef25a28b4673a1412905ea1d4548a8d675ba3
Aeva <aeva.ntsc@gmail.com>
2025-01-01 16:13:55 -0600

    less noisy startup

--- fnord.py
+++ fnord.py
@@ -1,5 +1,16 @@
 
 import os
+
+# Correct support of HiDPI on Linux requires setting both of these environment variables as well
+# as passing the desired unscaled resolution to `pygame.display.set_mode` via the `size` parameter.
+os.environ["SDL_VIDEODRIVER"] = "wayland,x11"
+os.environ["SDL_VIDEO_SCALE_METHOD"] = "letterbox"
+#os.environ["SDL_MOUSE_TOUCH_EVENTS"] = "1"
+
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
+import warnings
+warnings.simplefilter("ignore")
+
 import time
 import math
 from importlib import resources
@@ -75,11 +86,6 @@ if connection == "TiMidity":
 
 
 try:
-    # Correct support of HiDPI on Linux requires setting both of these environment variables as well
-    # as passing the desired unscaled resolution to `pygame.display.set_mode` via the `size` parameter.
-    os.environ["SDL_VIDEODRIVER"] = "wayland,x11"
-    os.environ["SDL_VIDEO_SCALE_METHOD"] = "letterbox"
-
     pygame.init()
 
     sizes = pygame.display.get_desktop_sizes()
