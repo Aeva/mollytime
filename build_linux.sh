041c42ed3fe96c9bbdec25bba13cf3fad14bf90c
Aeva <aeva.ntsc@gmail.com>
2025-06-22 19:35:56 -0500

    reorganizing

--- build_linux.sh
+++ build_linux.sh
@@ -2,7 +2,7 @@ cd src
 clang++ \
     -std=c++2c \
     -lm \
-    -I "glm-0.9.9.8" \
+    -I "../third_party/glm-0.9.9.8" \
     $(python -m pybind11 --includes) \
     -fPIC -shared \
     colors.cpp errors.cpp py_api.cpp \
