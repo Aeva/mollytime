61878f54751f3660232be1bf44147d68f3f51398
Aeva <aeva.ntsc@gmail.com>
2025-08-17 01:49:01 -0500

    I hate wayland with all my heart.

--- mollytime/__main__.py
+++ mollytime/__main__.py
@@ -42,37 +42,40 @@ unscaled_display_size = pygame.display.list_modes(display=display_index)[0]
 
 screen = pygame.display.set_mode(size=unscaled_display_size, display=display_index, flags=pygame.FULLSCREEN)
 
-xrandr_dpi = {}
+xrandr_dpi = None
 screen_index = 0
-while True:
-    xrandr = subprocess.run(("xrandr", "--screen", str(screen_index)), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    if xrandr.returncode == 0:
-        try:
-            report = xrandr.stdout.decode()
-            regex = r"Screen (\d+):.+current (\d+) x (\d+).+\n.+ (\d+)mm x (\d+)mm"
-            found = [list(map(int, r)) for r in re.findall(regex, report)]
-            assert(len(found) == 1)
-            reported_index, res_x, res_y, mm_x, mm_y = list(map(int, found[0]))
 
-            # xrandr may report the scaled or unscaled resolution, and so it cannot be relied upon for DPI calculation
-            res_x, res_y = sizes[screen_index]
+xrandr = subprocess.run(("xrandr", "--listactivemonitors"), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+if xrandr.returncode == 0:
+    try:
+        report = xrandr.stdout.decode().split("\n")
+        regex = r'^Monitors: (\d)$'
+        found = re.findall(regex, report[0], re.M)
+        assert(len(found) == 1)
+        assert(int(found[0]) == len(sizes))
 
-            in_x = mm_x / 25.4
-            in_y = mm_y / 25.4
-            dpi_x = res_x / in_x
-            dpi_y = res_y / in_y
-            xrandr_dpi[screen_index] = round((dpi_x + dpi_y) / 2)
-            screen_index += 1
-        except AssertionError:
-            break
-    else:
-        break
+        monitor_info = report[1 + display_index]
+        regex = r'^\s+(\d+):.+ (\d+)/(\d+)x(\d+)/(\d+)'
+        found = re.findall(regex, monitor_info, re.M)
+        assert(len(found) == 1)
+        reported_index, res_x, mm_x, res_y, mm_y = list(map(int, found[0]))
+        #print(f"monitor {display_index}: {res_x}x{res_y} ({mm_x}mm by {mm_y}mm)")
 
-if len(xrandr_dpi.keys()) != len(sizes):
-    print("Number of screens reported by xrandr does not match the number of screens reported by pygame!")
-    xrandr_dpi = {}
+        assert(len(found) == 1)
 
-dpi = xrandr_dpi.get(display_index, None)
+        # xrandr may report the scaled or unscaled resolution, and so it cannot be relied upon for DPI calculation
+        res_x, res_y = sizes[screen_index]
+
+        in_x = mm_x / 25.4
+        in_y = mm_y / 25.4
+        dpi_x = res_x / in_x
+        dpi_y = res_y / in_y
+        xrandr_dpi = round((dpi_x + dpi_y) / 2)
+        screen_index += 1
+    except AssertionError:
+        xrandr_dpi = None
+
+dpi = xrandr_dpi
 
 if dpi is None:
     print("Unable to calculate screen DPI via xrandr!")
--- mollytime/pygame_setup.py
+++ mollytime/pygame_setup.py
@@ -21,7 +21,18 @@ operating_system = platform.system()
 # Correct support of HiDPI on Linux requires setting both of these environment variables as well
 # as passing the desired unscaled resolution to `pygame.display.set_mode` via the `size` parameter.
 if operating_system == "Linux":
-	os.environ["SDL_VIDEODRIVER"] = "wayland,x11"
+    import re, subprocess
+    xrandr = subprocess.run(("xrandr", "--listactivemonitors"), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    if xrandr.returncode == 0:
+        report = xrandr.stdout.decode().split("\n")
+        regex = r'^Monitors: (\d)$'
+        found = re.findall(regex, report[0], re.M)
+        assert(len(found) == 1)
+        if found[0] == 1:
+            os.environ["SDL_VIDEODRIVER"] = "wayland,x11"
+        else:
+            # Wayland fucking broke multiple monitor support it is 2025 and I cannot fucking believe it.
+            os.environ["SDL_VIDEODRIVER"] = "x11,wayland"
 
 os.environ["SDL_VIDEO_SCALE_METHOD"] = "letterbox"
 #os.environ["SDL_MOUSE_TOUCH_EVENTS"] = "1"
