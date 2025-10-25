006c2d9211a1eb565f30b48ca3689821e1a3d336
Aeva <aeva.ntsc@gmail.com>
2025-10-25 16:56:38 -0500

    circle drawing

--- src/sdl_draw.cpp
+++ src/sdl_draw.cpp
@@ -3,10 +3,15 @@
 
 #include <SDL3/SDL_render.h>
 
+#include <cmath>
+#include <numbers>
 #include <algorithm>
 #include <cassert>
 #include <format>
 #include <stdexcept>
+
+
+constexpr double Tau = std::numbers::pi * 2.0;
 
 namespace Draw
 {
@@ -307,9 +312,95 @@ namespace Draw
         }
     }
 
-    void DrawCircle(Texture& Texture, const ColorPoint& Color, const Point& Center, int Radius, float Alpha)
+    static void PrepareConvexHull(
+        const std::vector<Point>& Points, const ColorPoint& Color, float Alpha,
+        std::vector<SDL_Vertex>& OutVertices, std::vector<int>& OutIndices)
     {
-        // TODO: Implement 😔
+        OutVertices.clear();
+        OutIndices.clear();
+        if (Points.size() == 0)
+        {
+            return;
+        }
+        {
+            glm::vec2 Center(0.0f, 0.0f);
+            OutVertices.reserve(Points.size() + 1);
+            {
+                // Placeholder for the center vertex, which is calculated later.
+                SDL_Vertex CenterVertexTBD;
+                Color.Eval(ColorSpace::sRGB, CenterVertexTBD.color);
+                CenterVertexTBD.color.a = Alpha;
+                OutVertices.push_back(CenterVertexTBD);
+            }
+            for (const Point& Corner : Points)
+            {
+                SDL_Vertex Vertex;
+                glm::vec2 Pt = GLMPoint(Corner);
+                Center += Pt;
+                Vertex.position = SDLPoint(Pt);
+                Vertex.color = OutVertices[0].color;
+                OutVertices.push_back(Vertex);
+            }
+            Center /= float(Points.size());
+            OutVertices[0].position = SDLPoint(Center);
+        }
+        {
+            glm::vec3 Center(OutVertices[0].position.x, OutVertices[0].position.y, 0.0f);
+            OutIndices.reserve(Points.size() * 3);
+            for (int IndexA = 0; IndexA < (int)Points.size(); ++IndexA)
+            {
+                int IndexB = (IndexA + 1) % (int)Points.size();
+
+                glm::vec3 PointA = glm::vec3(OutVertices[IndexA].position.x, OutVertices[IndexA].position.y, 0.0f) - Center;
+                glm::vec3 PointB = glm::vec3(OutVertices[IndexB].position.x, OutVertices[IndexB].position.y, 0.0f) - Center;
+                glm::vec3 Norm = glm::cross(PointA, PointB);
+                if (Norm.z < 0.0f)
+                {
+                    OutIndices.push_back(IndexA + 1);
+                    OutIndices.push_back(IndexB + 1);
+                }
+                else
+                {
+                    OutIndices.push_back(IndexB + 1);
+                    OutIndices.push_back(IndexA + 1);
+                }
+                OutIndices.push_back(0);
+            }
+        }
+    }
+
+    static std::vector<Point> UnitCircle(const int Count)
+    {
+        std::vector<Point> Points;
+        Points.resize(Count);
+        for (int Index = 0; Index < Count; ++Index)
+        {
+            const float Alpha = float(Index) / float(Count);
+            const float Angle = Tau * Alpha;
+            Points[Index] = { std::cos(Angle), -std::sin(Angle) };
+        }
+        return Points;
+    }
+
+    void DrawCircle(Texture& Texture, const ColorPoint& Color, const Point& Center, float Radius, float Alpha)
+    {
+        static std::vector<Point> CircleTemplate = UnitCircle(8);
+        std::vector<Point> Points;
+        Points.reserve(CircleTemplate.size());
+        const auto [CenterX, CenterY] = Center;
+        for (const auto [UnitX, UnitY] : CircleTemplate)
+        {
+            Points.emplace_back(UnitX * Radius + CenterX, UnitY * Radius + CenterY);
+        }
+
+        std::vector<SDL_Vertex> Vertices;
+        std::vector<int> Indices;
+        PrepareConvexHull(Points, Color, Alpha, Vertices, Indices);
+
+        if (!SDL_RenderGeometry(Renderer, &Texture.GetTexture(), Vertices.data(), Vertices.size(), Indices.data(), Indices.size()))
+        {
+            throw std::runtime_error(std::format("Failed to render circle. SDL error: {}", SDL_GetError()));
+        }
     }
 
     void DrawPolygon(Texture& Texture, const ColorPoint& Color, const std::vector<Point>& Vertices, float Alpha)
