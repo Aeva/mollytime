2193a0a40c5e1a3587eebfa1f59f1e00022c30e4
Aeva <aeva.ntsc@gmail.com>
2025-10-25 13:35:22 -0500

    Changes to get the SDL3 branch building on Linux

--- src/sdl_time.cpp
+++ src/sdl_time.cpp
@@ -16,7 +16,8 @@ namespace Time
 
         const uint64_t FramerateInterval = (1.0 / Framerate) * 1e+9;
         const uint64_t TimeSinceLastTick = SDL_GetTicksNS() - this->LastTick;
-        const uint64_t TimeToDelay = std::max(0llu, FramerateInterval - TimeSinceLastTick);
+        const uint64_t Zero = 0;
+        const uint64_t TimeToDelay = std::max(Zero, FramerateInterval - TimeSinceLastTick);
 
         SDL_DelayPrecise(TimeToDelay);
     }
