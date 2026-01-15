f9dc0a317c9a9911018929c8b467d2d047eda236
Clinkscales <>
2026-01-15 13:52:48 -0600

    Add submodule helper
    The main work here is automatically processing the magic incantations for getting (only) the Boost modules required by the project. (See: https://www.boost.org/doc/user-guide/getting-started.html#_individual_modules)

---
+++ build_tools/get_submodules.py
@@ -0,0 +1,59 @@
+import os
+import shutil
+import subprocess
+import sys
+
+from pathlib import Path
+
+def _get_boost(desired_boost_modules: list[str]):
+    get_submodule_args = [ "git", "submodule", "update", "--depth", "1", "-q", "--init" ]
+    boost_depinst_path = Path("tools/boostdep/depinst/depinst.py")
+
+    # Boost tools need the boost directory to be current working directory.
+    this_dir = Path(__file__).parent
+    project_dir = this_dir.parent
+    boost_dir = project_dir / "third_party" / "boost_1_90_0"
+
+    old_cwd = os.getcwd()
+    try:
+        os.chdir(boost_dir)
+
+        # Acquire the boost dependency manager.
+        get_boostdep_process = subprocess.run(get_submodule_args + [ "tools/boostdep" ])
+        get_boostdep_process.check_returncode()
+
+        for module in desired_boost_modules:
+            # Acquire this module.
+            get_module_process = subprocess.run(get_submodule_args + [ f"libs/{module}" ])
+            get_module_process.check_returncode()
+
+            # Install its dependencies.
+            # (Yes, that's correct, "--depth 1" is a single argument. `depinst.py` appends this verbatim to a git command line.)
+            dep_install_process = subprocess.run([ sys.executable, boost_depinst_path, "-X", "test", "-g", "--depth 1", module ])
+            dep_install_process.check_returncode()
+    
+    finally:
+        os.chdir(old_cwd)
+
+# This is all git stuff.
+if shutil.which("git") == None:
+    raise FileNotFoundError("Can't find 'git' on PATH. I need Git to get submodules.")
+
+print("I'm going to initialize & acquire all submodules. This'll take a minute...\n")
+
+# Switch to the project root.
+this_dir = Path(__file__).parent
+project_dir = this_dir.parent
+os.chdir(project_dir)
+
+# Init and update base submodules.
+init_process = subprocess.run([ "git", "submodule", "init" ])
+init_process.check_returncode()
+
+update_process = subprocess.run([ "git", "submodule", "update" ])
+update_process.check_returncode()
+
+# Gather boost submodules, and their dependencies.
+_get_boost([ "atomic", "stacktrace" ])
+
+print("\n...OK, looks like I've got everything!")
