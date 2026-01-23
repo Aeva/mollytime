4d3dc9b3a4eb5b837212de4ba99bbb9a921dbff4
Clinkscales <>
2026-01-23 14:36:29 -0600

    Implement SDL audio backend
    Thanks to the fine work of the SDL team, this is by far the simplest backend. There's *almost* no "extra work" required on our part: we open a stream, wait for a callback to request more audio, then directly pass in our samples.
    
    The quirks are:
    - SDL's handy one-and-done helper for a simple single-object audio stream, which currently fits our use case perfectly, returns a "paused" stream, which must be manually resumed. This is apparently solely for SDL2 API compatibility.
    - SDL can't provide a guaranteed fixed-size output buffer; samples required may vary. So we need our intermediary buffers to be able to resize themselves to whatever the next high-water mark is. This is not expected to happen frequently, if at all -- on my machine, it's an exact 3840 samples every time -- so I provide a best-guess starting capacity of 5000 to hopefully ward off any actual need to resize.
    - SDL requires interleaved data, but our data is separated per-channel. SDL can, helpfully, do the interleaving for us, which is handy; but this might be slower than its fastest API option of directly reading from an already-interleaved buffer. This might signal a future optimization of pre-interleaving generated samples, so neither SDL nor our backend needs to perform this step. (WASAPI also requires interleaved, which we perform manually, FWIW.)
    
    Since this is now the most-compatible backend (we vendor SDL, and it supports tons of platforms), I've listed it first in the build options, to signal it's the "use this if you don't have a better idea" case. This does of course mean it depends on our SDL target, which required reorganizing the build file slightly.

---
+++ src/sdl_stream.h
@@ -0,0 +1,58 @@
+// Copyright 2025 Aeva Palecek
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#ifdef AUDIO_SDL
+
+#include "audio_backend.h"
+
+struct SDL_AudioStream;
+
+
+struct SDLThreadShared final : AudioThreadShared
+{
+    std::vector<float> OutputSamplesLeft;
+    std::vector<float> OutputSamplesRight;
+};
+
+
+class SDLRealTimeThread final : public RealTimeAudioThread
+{
+    SDL_AudioStream* SDLStream;
+
+    static void OnAudioRequested(void* UserData, SDL_AudioStream* Stream, int AdditionalAmount, int TotalAmount);
+
+public:
+    SDLRealTimeThread(SDLThreadShared* WasapiBufferState, int SampleRate);
+    virtual ~SDLRealTimeThread() override;
+
+    virtual void BeginFrame(FramePointers& Frame) override;
+    virtual void EndFrame(FramePointers& Frame) override;
+};
+
+
+class SDLStream final : public AudioStream
+{
+    SDLThreadShared BufferState;
+    SDLRealTimeThread RealTimeThread;
+
+public:
+    SDLStream(int SampleRate);
+
+    virtual float GetTemporalPressure() override;
+    virtual void ProgramChange(ScratchUniquePtr&& NewProgram) override;
+};
+
+#endif
