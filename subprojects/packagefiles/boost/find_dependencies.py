2f6bbd83a57cfb592376439f94d938d84407301d
Clinkscales <>
2026-01-31 14:23:04 -0600

    Add custom wrap for Boost (headers-only, libs hardcoded)
    There are two concerns here:
    - Ensuring that we *only* get as much Boost as we need.
    - Ensuring that Boost dependencies are correctly to provided to Meson, in a way it can consistently reproduce.
    
    Boost's new modular dependency system -- which we've already been using -- works by leveraging Git submodules, thus implicitly making the build depend on Git.
    
    With that being the case, it's simplest to let Meson acquire Boost through its `[wrap-git]` feature. Like before, we're taking the latest stable tag (1.90.0), shallow-cloned, without submodules. Then, like before, we can then acquire the requested modules using a local helper script, which I've lightly adapted to work inline as a build command, `get_boost.py`. For now, the desired modules are hardcoded, but this Should™ be easy to turn into a generic build option as a follow-up change.
    
    That leaves patching into Meson.
    
    Boost does not use the Meson build system. Boost will never use the Meson build system. The Meson build system is incapable of properly wrapping Boost un-aided. The Meson team and their fans are uninterested and possibly incapable of properly wrapping Boost as a whole -- a couple of individual modules have wraps available (e.g. `asio`), but not the ones we need. Boost provides a CMake option, but Meson's CMake module remains woefully inadequate, failing to even start when invoked on the Boost project. Manually writing An Entire Alternate Build System for Boost ourselves is laughably, stupidly prohibitive.
    
    This means -- despite Meson's regular insistence that mixing build systems is bad and everything everywhere should be RIIR-ed in pure clean perfect flawless lemony-fresh Meson without exception, as a fundamental moral imperative -- we'd have to mix build systems to feasibly build Boost. The good news is that, for the moment, we don't actually require any build libraries from Boost, we just need headers. This lets us *avoid the problem*, for now.
    
    Boost standardizes header locations in `libs/<the_module>/include`, in all cases. This means that as long as we know which modules we need, we can generically add include paths for each one, and effectively get the same view as we'd get from a formal install.
    
    "As long as we know which modules we need" is, of course, the catch. Boost doesn't provide a quick-and-dirty system to *query* dependencies for requested modules, only to acquire them. However, this acquire-script (necessarily) contains the mechanisms to identify these modules, and due to no small amount of luck, it's a Python script. So I've added another helper script of our own that patches into Boost's dependency-getter, extracts the dependency-finding logic, and prints the results into a line-delimited list that Meson can, thus, easily parse. This is tricky, but reasonably concise, so check `find_dependencies.py` for details.
    
    Future: To also build libraries, it'd almost certainly be best to patch into Boost's build system (B2). Since we already know how to find requested modules, we can pass those modules into B2, along with any intermediary or output dir required by Meson. The difficult parts will be:
    1. *Identifying all output files* that will be emitted by the build. I don't think B2 can emit these up-front, so they'd have to be interpreted, inferred, or hardcoded.
    2. *Organizing all output files* emitted by the build in a Meson-approved fashion. This means no subfolders, period.

---
+++ subprojects/packagefiles/boost/find_dependencies.py
@@ -0,0 +1,40 @@
+import os
+
+from argparse import ArgumentParser
+from pathlib import Path
+
+# We're going to patch into `depinst.py`, so we can hijack its module-scanner.
+from tools.boostdep.depinst.depinst import read_exceptions, read_gitmodules, scan_module_dependencies  # pyright: ignore[reportUnknownVariableType]
+
+parser = ArgumentParser()
+_ = parser.add_argument("modules", nargs = "+", help = "Modules to scan for dependencies.")
+
+args = parser.parse_args()
+modules: list[str] = args.modules  # pyright: ignore[reportAny]
+
+# `depinst.py` hard-assumes that it's being invoked directly (via reading `sys.argv[0]`).
+# We need to trick its subsequent path calculations into being correct by manipulating the CWD.
+boost_dir = Path(__file__).parent
+depinst_dir = boost_dir / 'tools' / 'boostdep' / 'depinst'
+
+# Read exceptions relative to `depinst.py`.
+os.chdir(depinst_dir)
+exceptions: dict[str, str] = read_exceptions()  # pyright: ignore[reportUnknownVariableType]
+
+# Read gitmodules relative to the Boost tree.
+os.chdir(boost_dir)
+git_modules: list[str] = read_gitmodules()  # pyright: ignore[reportUnknownVariableType]
+
+# OK, we're ready to scan for dependencies.
+# Refer to `depinst.py` for these values, just in case they change in the future.
+modules_always_required = [ 'config', 'headers' ]   # `./tools` modules are omitted, as they have no installed library representatioon.
+scan_dirs = [ 'include', 'src' ]
+dependencies: dict[str, int] = {}
+
+for module in modules_always_required + modules:
+    dependencies[module] = 1
+    scan_module_dependencies(module, exceptions, git_modules, dependencies, scan_dirs, [])
+
+# We don't care if they're installed or not. Just print them out as a line-separated list.
+for module in dependencies.keys():
+    print(module)
