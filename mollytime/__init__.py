e103d111c4f0d00b18296d004e8baceb003a0ec2
Clinkscales <>
2026-02-01 18:31:31 -0600

    Only perform DLL directory wrangling on Windows
    Turns out this wasn't harmless. Despite being in a generic `os` module, `add_dll_directory()` doesn't exist on other platforms, causing an attribute error.

--- mollytime/__init__.py
+++ mollytime/__init__.py
@@ -3,10 +3,11 @@ import site
 
 from pathlib import Path
 
-# Meson-python will stick packaged wheel DLLs in a top-level site-packages location.
-# Look through all site-packages directories to see if we can find it.
-all_site_packages_dirs = site.getsitepackages() + [ site.getusersitepackages() ]
-for site_package_dir in all_site_packages_dirs:
-    maybe_library_dir = Path(site_package_dir).resolve() / ".mollytime.mesonpy.libs"
-    if maybe_library_dir.exists():
-        _ = os.add_dll_directory(str(maybe_library_dir))
+if os.name == "nt":
+    # Meson-python will stick packaged wheel DLLs in a top-level site-packages location.
+    # Look through all site-packages directories to see if we can find it.
+    all_site_packages_dirs = site.getsitepackages() + [ site.getusersitepackages() ]
+    for site_package_dir in all_site_packages_dirs:
+        maybe_library_dir = Path(site_package_dir).resolve() / ".mollytime.mesonpy.libs"
+        if maybe_library_dir.exists():
+            _ = os.add_dll_directory(str(maybe_library_dir))
